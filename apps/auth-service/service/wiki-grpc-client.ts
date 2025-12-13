2c15526782ed986472090489b5f77212c3bd5549
jerrylingj <498539230@qq.com>
2025-12-13 11:29:14 +0800

    feat: 更新metadata, 支持传递jwt

--- apps/auth-service/service/wiki-grpc-client.ts
+++ apps/auth-service/service/wiki-grpc-client.ts
@@ -1,3 +1,4 @@
+import type { Metadata } from '@grpc/grpc-js'
 import type { RpcClientConfig } from '@sse-wiki/rpc-client'
 import type * as ArticleServiceTypes from '../protobuf/types/article_service'
 import type * as DiscussionServiceTypes from '../protobuf/types/discussion_service'
@@ -6,6 +7,9 @@ import type * as ReviewServiceTypes from '../protobuf/types/review_service'
 import path from 'node:path'
 import process from 'node:process'
 import { RpcClient } from '@sse-wiki/rpc-client'
+
+// Re-export Metadata for convenience
+export { Metadata } from '@sse-wiki/rpc-client'
 
 /**
  * 服务客户端配置
@@ -37,44 +41,44 @@ export class ModuleServiceClient {
     })
   }
 
-  async GetModuleTree(req: ModuleServiceTypes.GetModuleTreeRequest): Promise<ModuleServiceTypes.GetModuleTreeResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.GetModuleTreeRequest, ModuleServiceTypes.GetModuleTreeResponse>('GetModuleTree', req)
+  async GetModuleTree(req: ModuleServiceTypes.GetModuleTreeRequest, metadata?: Metadata): Promise<ModuleServiceTypes.GetModuleTreeResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.GetModuleTreeRequest, ModuleServiceTypes.GetModuleTreeResponse>('GetModuleTree', req, metadata)
   }
 
-  async GetModule(req: ModuleServiceTypes.GetModuleRequest): Promise<ModuleServiceTypes.GetModuleResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.GetModuleRequest, ModuleServiceTypes.GetModuleResponse>('GetModule', req)
+  async GetModule(req: ModuleServiceTypes.GetModuleRequest, metadata?: Metadata): Promise<ModuleServiceTypes.GetModuleResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.GetModuleRequest, ModuleServiceTypes.GetModuleResponse>('GetModule', req, metadata)
   }
 
-  async GetBreadcrumbs(req: ModuleServiceTypes.GetBreadcrumbsRequest): Promise<ModuleServiceTypes.GetBreadcrumbsResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.GetBreadcrumbsRequest, ModuleServiceTypes.GetBreadcrumbsResponse>('GetBreadcrumbs', req)
+  async GetBreadcrumbs(req: ModuleServiceTypes.GetBreadcrumbsRequest, metadata?: Metadata): Promise<ModuleServiceTypes.GetBreadcrumbsResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.GetBreadcrumbsRequest, ModuleServiceTypes.GetBreadcrumbsResponse>('GetBreadcrumbs', req, metadata)
   }
 
-  async CreateModule(req: ModuleServiceTypes.CreateModuleRequest): Promise<ModuleServiceTypes.CreateModuleResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.CreateModuleRequest, ModuleServiceTypes.CreateModuleResponse>('CreateModule', req)
+  async CreateModule(req: ModuleServiceTypes.CreateModuleRequest, metadata?: Metadata): Promise<ModuleServiceTypes.CreateModuleResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.CreateModuleRequest, ModuleServiceTypes.CreateModuleResponse>('CreateModule', req, metadata)
   }
 
-  async UpdateModule(req: ModuleServiceTypes.UpdateModuleRequest): Promise<ModuleServiceTypes.UpdateModuleResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.UpdateModuleRequest, ModuleServiceTypes.UpdateModuleResponse>('UpdateModule', req)
+  async UpdateModule(req: ModuleServiceTypes.UpdateModuleRequest, metadata?: Metadata): Promise<ModuleServiceTypes.UpdateModuleResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.UpdateModuleRequest, ModuleServiceTypes.UpdateModuleResponse>('UpdateModule', req, metadata)
   }
 
-  async DeleteModule(req: ModuleServiceTypes.DeleteModuleRequest): Promise<ModuleServiceTypes.DeleteModuleResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.DeleteModuleRequest, ModuleServiceTypes.DeleteModuleResponse>('DeleteModule', req)
+  async DeleteModule(req: ModuleServiceTypes.DeleteModuleRequest, metadata?: Metadata): Promise<ModuleServiceTypes.DeleteModuleResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.DeleteModuleRequest, ModuleServiceTypes.DeleteModuleResponse>('DeleteModule', req, metadata)
   }
 
-  async GetModerators(req: ModuleServiceTypes.GetModeratorsRequest): Promise<ModuleServiceTypes.GetModeratorsResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.GetModeratorsRequest, ModuleServiceTypes.GetModeratorsResponse>('GetModerators', req)
+  async GetModerators(req: ModuleServiceTypes.GetModeratorsRequest, metadata?: Metadata): Promise<ModuleServiceTypes.GetModeratorsResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.GetModeratorsRequest, ModuleServiceTypes.GetModeratorsResponse>('GetModerators', req, metadata)
   }
 
-  async AddModerator(req: ModuleServiceTypes.AddModeratorRequest): Promise<ModuleServiceTypes.AddModeratorResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.AddModeratorRequest, ModuleServiceTypes.AddModeratorResponse>('AddModerator', req)
+  async AddModerator(req: ModuleServiceTypes.AddModeratorRequest, metadata?: Metadata): Promise<ModuleServiceTypes.AddModeratorResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.AddModeratorRequest, ModuleServiceTypes.AddModeratorResponse>('AddModerator', req, metadata)
   }
 
-  async RemoveModerator(req: ModuleServiceTypes.RemoveModeratorRequest): Promise<ModuleServiceTypes.RemoveModeratorResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.RemoveModeratorRequest, ModuleServiceTypes.RemoveModeratorResponse>('RemoveModerator', req)
+  async RemoveModerator(req: ModuleServiceTypes.RemoveModeratorRequest, metadata?: Metadata): Promise<ModuleServiceTypes.RemoveModeratorResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.RemoveModeratorRequest, ModuleServiceTypes.RemoveModeratorResponse>('RemoveModerator', req, metadata)
   }
 
-  async HandleLock(req: ModuleServiceTypes.HandleLockRequest): Promise<ModuleServiceTypes.HandleLockResponse> {
-    return this.rpcClient.call<ModuleServiceTypes.HandleLockRequest, ModuleServiceTypes.HandleLockResponse>('HandleLock', req)
+  async HandleLock(req: ModuleServiceTypes.HandleLockRequest, metadata?: Metadata): Promise<ModuleServiceTypes.HandleLockResponse> {
+    return this.rpcClient.call<ModuleServiceTypes.HandleLockRequest, ModuleServiceTypes.HandleLockResponse>('HandleLock', req, metadata)
   }
 
   close(): void {
@@ -104,56 +108,60 @@ export class ArticleServiceClient {
     })
   }
 
-  async GetArticlesByModule(req: ArticleServiceTypes.GetArticlesByModuleRequest): Promise<ArticleServiceTypes.GetArticlesByModuleResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.GetArticlesByModuleRequest, ArticleServiceTypes.GetArticlesByModuleResponse>('GetArticlesByModule', req)
+  async GetArticlesByModule(req: ArticleServiceTypes.GetArticlesByModuleRequest, metadata?: Metadata): Promise<ArticleServiceTypes.GetArticlesByModuleResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.GetArticlesByModuleRequest, ArticleServiceTypes.GetArticlesByModuleResponse>('GetArticlesByModule', req, metadata)
   }
 
-  async GetArticle(req: ArticleServiceTypes.GetArticleRequest): Promise<ArticleServiceTypes.GetArticleResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.GetArticleRequest, ArticleServiceTypes.GetArticleResponse>('GetArticle', req)
+  async GetArticle(req: ArticleServiceTypes.GetArticleRequest, metadata?: Metadata): Promise<ArticleServiceTypes.GetArticleResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.GetArticleRequest, ArticleServiceTypes.GetArticleResponse>('GetArticle', req, metadata)
   }
 
-  async GetVersions(req: ArticleServiceTypes.GetVersionsRequest): Promise<ArticleServiceTypes.GetVersionsResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.GetVersionsRequest, ArticleServiceTypes.GetVersionsResponse>('GetVersions', req)
+  async GetVersions(req: ArticleServiceTypes.GetVersionsRequest, metadata?: Metadata): Promise<ArticleServiceTypes.GetVersionsResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.GetVersionsRequest, ArticleServiceTypes.GetVersionsResponse>('GetVersions', req, metadata)
   }
 
-  async GetVersion(req: ArticleServiceTypes.GetVersionRequest): Promise<ArticleServiceTypes.GetVersionResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.GetVersionRequest, ArticleServiceTypes.GetVersionResponse>('GetVersion', req)
+  async GetVersion(req: ArticleServiceTypes.GetVersionRequest, metadata?: Metadata): Promise<ArticleServiceTypes.GetVersionResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.GetVersionRequest, ArticleServiceTypes.GetVersionResponse>('GetVersion', req, metadata)
   }
 
-  async GetVersionDiff(req: ArticleServiceTypes.GetVersionDiffRequest): Promise<ArticleServiceTypes.GetVersionDiffResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.GetVersionDiffRequest, ArticleServiceTypes.GetVersionDiffResponse>('GetVersionDiff', req)
+  async GetVersionDiff(req: ArticleServiceTypes.GetVersionDiffRequest, metadata?: Metadata): Promise<ArticleServiceTypes.GetVersionDiffResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.GetVersionDiffRequest, ArticleServiceTypes.GetVersionDiffResponse>('GetVersionDiff', req, metadata)
   }
 
-  async GetUserArticleFavourites(req: ArticleServiceTypes.GetArticleFavouritesRequest): Promise<ArticleServiceTypes.GetArticleFavouritesResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.GetArticleFavouritesRequest, ArticleServiceTypes.GetArticleFavouritesResponse>('GetUserArticleFavourites', req)
+  async GetUserArticleFavourites(req: ArticleServiceTypes.GetArticleFavouritesRequest, metadata?: Metadata): Promise<ArticleServiceTypes.GetArticleFavouritesResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.GetArticleFavouritesRequest, ArticleServiceTypes.GetArticleFavouritesResponse>('GetUserArticleFavourites', req, metadata)
   }
 
-  async UpdateUserFavourites(req: ArticleServiceTypes.UpdateUserFavouritesRequest): Promise<ArticleServiceTypes.UpdateUserFavouritesResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.UpdateUserFavouritesRequest, ArticleServiceTypes.UpdateUserFavouritesResponse>('UpdateUserFavourites', req)
+  async UpdateUserFavourites(req: ArticleServiceTypes.UpdateUserFavouritesRequest, metadata?: Metadata): Promise<ArticleServiceTypes.UpdateUserFavouritesResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.UpdateUserFavouritesRequest, ArticleServiceTypes.UpdateUserFavouritesResponse>('UpdateUserFavourites', req, metadata)
   }
 
-  async CreateArticle(req: ArticleServiceTypes.CreateArticleRequest): Promise<ArticleServiceTypes.CreateArticleResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.CreateArticleRequest, ArticleServiceTypes.CreateArticleResponse>('CreateArticle', req)
+  async CreateArticle(req: ArticleServiceTypes.CreateArticleRequest, metadata?: Metadata): Promise<ArticleServiceTypes.CreateArticleResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.CreateArticleRequest, ArticleServiceTypes.CreateArticleResponse>('CreateArticle', req, metadata)
   }
 
-  async CreateSubmission(req: ArticleServiceTypes.CreateSubmissionRequest): Promise<ArticleServiceTypes.CreateSubmissionResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.CreateSubmissionRequest, ArticleServiceTypes.CreateSubmissionResponse>('CreateSubmission', req)
+  async CreateSubmission(req: ArticleServiceTypes.CreateSubmissionRequest, metadata?: Metadata): Promise<ArticleServiceTypes.CreateSubmissionResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.CreateSubmissionRequest, ArticleServiceTypes.CreateSubmissionResponse>('CreateSubmission', req, metadata)
   }
 
-  async UpdateBasicInfo(req: ArticleServiceTypes.UpdateBasicInfoRequest): Promise<ArticleServiceTypes.UpdateBasicInfoResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.UpdateBasicInfoRequest, ArticleServiceTypes.UpdateBasicInfoResponse>('UpdateBasicInfo', req)
+  async UpdateBasicInfo(req: ArticleServiceTypes.UpdateBasicInfoRequest, metadata?: Metadata): Promise<ArticleServiceTypes.UpdateBasicInfoResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.UpdateBasicInfoRequest, ArticleServiceTypes.UpdateBasicInfoResponse>('UpdateBasicInfo', req, metadata)
   }
 
-  async GetCollaborators(req: ArticleServiceTypes.GetCollaboratorsRequest): Promise<ArticleServiceTypes.GetCollaboratorsResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.GetCollaboratorsRequest, ArticleServiceTypes.GetCollaboratorsResponse>('GetCollaborators', req)
+  async GetCollaborators(req: ArticleServiceTypes.GetCollaboratorsRequest, metadata?: Metadata): Promise<ArticleServiceTypes.GetCollaboratorsResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.GetCollaboratorsRequest, ArticleServiceTypes.GetCollaboratorsResponse>('GetCollaborators', req, metadata)
   }
 
-  async AddCollaborator(req: ArticleServiceTypes.AddCollaboratorRequest): Promise<ArticleServiceTypes.AddCollaboratorResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.AddCollaboratorRequest, ArticleServiceTypes.AddCollaboratorResponse>('AddCollaborator', req)
+  async AddCollaborator(req: ArticleServiceTypes.AddCollaboratorRequest, metadata?: Metadata): Promise<ArticleServiceTypes.AddCollaboratorResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.AddCollaboratorRequest, ArticleServiceTypes.AddCollaboratorResponse>('AddCollaborator', req, metadata)
   }
 
-  async RemoveCollaborator(req: ArticleServiceTypes.RemoveCollaboratorRequest): Promise<ArticleServiceTypes.RemoveCollaboratorResponse> {
-    return this.rpcClient.call<ArticleServiceTypes.RemoveCollaboratorRequest, ArticleServiceTypes.RemoveCollaboratorResponse>('RemoveCollaborator', req)
+  async RemoveCollaborator(req: ArticleServiceTypes.RemoveCollaboratorRequest, metadata?: Metadata): Promise<ArticleServiceTypes.RemoveCollaboratorResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.RemoveCollaboratorRequest, ArticleServiceTypes.RemoveCollaboratorResponse>('RemoveCollaborator', req, metadata)
+  }
+
+  async DeleteArticle(req: ArticleServiceTypes.DeleteArticleRequest, metadata?: Metadata): Promise<ArticleServiceTypes.DeleteArticleResponse> {
+    return this.rpcClient.call<ArticleServiceTypes.DeleteArticleRequest, ArticleServiceTypes.DeleteArticleResponse>('DeleteArticle', req, metadata)
   }
 
   close(): void {
@@ -183,16 +191,16 @@ export class ReviewServiceClient {
     })
   }
 
-  async GetReviews(req: ReviewServiceTypes.GetReviewsRequest): Promise<ReviewServiceTypes.GetReviewsResponse> {
-    return this.rpcClient.call<ReviewServiceTypes.GetReviewsRequest, ReviewServiceTypes.GetReviewsResponse>('GetReviews', req)
+  async GetReviews(req: ReviewServiceTypes.GetReviewsRequest, metadata?: Metadata): Promise<ReviewServiceTypes.GetReviewsResponse> {
+    return this.rpcClient.call<ReviewServiceTypes.GetReviewsRequest, ReviewServiceTypes.GetReviewsResponse>('GetReviews', req, metadata)
   }
 
-  async GetReviewDetail(req: ReviewServiceTypes.GetReviewDetailRequest): Promise<ReviewServiceTypes.GetReviewDetailResponse> {
-    return this.rpcClient.call<ReviewServiceTypes.GetReviewDetailRequest, ReviewServiceTypes.GetReviewDetailResponse>('GetReviewDetail', req)
+  async GetReviewDetail(req: ReviewServiceTypes.GetReviewDetailRequest, metadata?: Metadata): Promise<ReviewServiceTypes.GetReviewDetailResponse> {
+    return this.rpcClient.call<ReviewServiceTypes.GetReviewDetailRequest, ReviewServiceTypes.GetReviewDetailResponse>('GetReviewDetail', req, metadata)
   }
 
-  async ReviewAction(req: ReviewServiceTypes.ReviewActionRequest): Promise<ReviewServiceTypes.ReviewActionResponse> {
-    return this.rpcClient.call<ReviewServiceTypes.ReviewActionRequest, ReviewServiceTypes.ReviewActionResponse>('ReviewAction', req)
+  async ReviewAction(req: ReviewServiceTypes.ReviewActionRequest, metadata?: Metadata): Promise<ReviewServiceTypes.ReviewActionResponse> {
+    return this.rpcClient.call<ReviewServiceTypes.ReviewActionRequest, ReviewServiceTypes.ReviewActionResponse>('ReviewAction', req, metadata)
   }
 
   close(): void {
@@ -222,24 +230,24 @@ export class DiscussionServiceClient {
     })
   }
 
-  async GetArticleComments(req: DiscussionServiceTypes.GetArticleCommentsRequest): Promise<DiscussionServiceTypes.GetArticleCommentsResponse> {
-    return this.rpcClient.call<DiscussionServiceTypes.GetArticleCommentsRequest, DiscussionServiceTypes.GetArticleCommentsResponse>('GetArticleComments', req)
+  async GetArticleComments(req: DiscussionServiceTypes.GetArticleCommentsRequest, metadata?: Metadata): Promise<DiscussionServiceTypes.GetArticleCommentsResponse> {
+    return this.rpcClient.call<DiscussionServiceTypes.GetArticleCommentsRequest, DiscussionServiceTypes.GetArticleCommentsResponse>('GetArticleComments', req, metadata)
   }
 
-  async CreateComment(req: DiscussionServiceTypes.CreateCommentRequest): Promise<DiscussionServiceTypes.CreateCommentResponse> {
-    return this.rpcClient.call<DiscussionServiceTypes.CreateCommentRequest, DiscussionServiceTypes.CreateCommentResponse>('CreateComment', req)
+  async CreateComment(req: DiscussionServiceTypes.CreateCommentRequest, metadata?: Metadata): Promise<DiscussionServiceTypes.CreateCommentResponse> {
+    return this.rpcClient.call<DiscussionServiceTypes.CreateCommentRequest, DiscussionServiceTypes.CreateCommentResponse>('CreateComment', req, metadata)
   }
 
-  async ReplyComment(req: DiscussionServiceTypes.ReplyCommentRequest): Promise<DiscussionServiceTypes.ReplyCommentResponse> {
-    return this.rpcClient.call<DiscussionServiceTypes.ReplyCommentRequest, DiscussionServiceTypes.ReplyCommentResponse>('ReplyComment', req)
+  async ReplyComment(req: DiscussionServiceTypes.ReplyCommentRequest, metadata?: Metadata): Promise<DiscussionServiceTypes.ReplyCommentResponse> {
+    return this.rpcClient.call<DiscussionServiceTypes.ReplyCommentRequest, DiscussionServiceTypes.ReplyCommentResponse>('ReplyComment', req, metadata)
   }
 
-  async UpdateComment(req: DiscussionServiceTypes.UpdateCommentRequest): Promise<DiscussionServiceTypes.UpdateCommentResponse> {
-    return this.rpcClient.call<DiscussionServiceTypes.UpdateCommentRequest, DiscussionServiceTypes.UpdateCommentResponse>('UpdateComment', req)
+  async UpdateComment(req: DiscussionServiceTypes.UpdateCommentRequest, metadata?: Metadata): Promise<DiscussionServiceTypes.UpdateCommentResponse> {
+    return this.rpcClient.call<DiscussionServiceTypes.UpdateCommentRequest, DiscussionServiceTypes.UpdateCommentResponse>('UpdateComment', req, metadata)
   }
 
-  async DeleteComment(req: DiscussionServiceTypes.DeleteCommentRequest): Promise<DiscussionServiceTypes.DeleteCommentResponse> {
-    return this.rpcClient.call<DiscussionServiceTypes.DeleteCommentRequest, DiscussionServiceTypes.DeleteCommentResponse>('DeleteComment', req)
+  async DeleteComment(req: DiscussionServiceTypes.DeleteCommentRequest, metadata?: Metadata): Promise<DiscussionServiceTypes.DeleteCommentResponse> {
+    return this.rpcClient.call<DiscussionServiceTypes.DeleteCommentRequest, DiscussionServiceTypes.DeleteCommentResponse>('DeleteComment', req, metadata)
   }
 
   close(): void {
